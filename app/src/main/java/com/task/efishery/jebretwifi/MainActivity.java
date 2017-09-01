3ee690519f2c23ddeb3e25af9a63e0f130cea18a
srsugara <syauqisugara@gmail.com>
2017-09-01 16:26:28 +0700

    add configuration for fabric chraslytic

--- app/src/main/java/com/task/efishery/jebretwifi/MainActivity.java
+++ app/src/main/java/com/task/efishery/jebretwifi/MainActivity.java
@@ -7,6 +7,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 import it.neokree.materialtabs.MaterialTab;
 import it.neokree.materialtabs.MaterialTabHost;
 import it.neokree.materialtabs.MaterialTabListener;
@@ -21,6 +23,7 @@ public class MainActivity extends AppCompatActivity implements MaterialTabListen
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
 
         //android toolbar
