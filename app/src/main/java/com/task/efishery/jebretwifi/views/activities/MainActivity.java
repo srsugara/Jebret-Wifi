9d3738214a5ee3fafce6a56a23ece714369ac778
srsugara <syauqisugara@gmail.com>
2017-09-04 22:17:43 +0700

    add method for request permission

--- app/src/main/java/com/task/efishery/jebretwifi/views/activities/MainActivity.java
+++ app/src/main/java/com/task/efishery/jebretwifi/views/activities/MainActivity.java
@@ -1,16 +1,27 @@
 package com.task.efishery.jebretwifi.views.activities;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.provider.SyncStateContract;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.widget.Toast;
+
 import com.crashlytics.android.Crashlytics;
 import com.task.efishery.jebretwifi.views.fragments.QuoteFragment;
 import com.task.efishery.jebretwifi.R;
 import com.task.efishery.jebretwifi.views.components.TabMenuAdapter;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -29,6 +40,9 @@ public class MainActivity extends AppCompatActivity implements MaterialTabListen
     Toolbar toolBar;
 
     TabMenuAdapter androidAdapter;
+    public static final int ACCESS_COARSE_LOCATION = 100;
+    public static final int CAMERA = 101;
+    public static final int ACCESS_FINE_LOCATION = 102;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -58,8 +72,78 @@ public class MainActivity extends AppCompatActivity implements MaterialTabListen
                             .setTabListener(this)
             );
         }
+
+        doCheckPermissions();
     }
 
+    /**
+     * Check permission
+     */
+    private void doCheckPermissions() {
+        if (isAccessesGranted()) {
+            gettingPermissions();
+        }
+    }
+
+    /**
+     * Check if our app has all required access
+     * @return boolean
+     */
+    private boolean isAccessesGranted() {
+        int granted = PackageManager.PERMISSION_GRANTED;
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != granted
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != granted
+                || ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != granted;
+    }
+
+    /**
+     * Get required permission
+     */
+    private void gettingPermissions() {
+        Map<String, Integer> permissions = new HashMap<>();
+        permissions.put(Manifest.permission.ACCESS_COARSE_LOCATION, ACCESS_COARSE_LOCATION);
+        permissions.put(Manifest.permission.CAMERA, CAMERA);
+        permissions.put(Manifest.permission.ACCESS_FINE_LOCATION, ACCESS_FINE_LOCATION);
+
+        for (String permission : permissions.keySet()) {
+            if (ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this, permission)) {
+                    Toast.makeText(MainActivity.this,"getting permission",Toast.LENGTH_LONG).show();
+                }
+                requestAccess(permission, permissions.get(permission));
+                break;
+            }
+        }
+    }
+
+    /**
+     * Request permission to system
+     * @param permission
+     * @param requestCode
+     */
+    private void requestAccess(String permission, int requestCode) {
+        ActivityCompat.requestPermissions(this, new String[]{permission}, requestCode );
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        switch (requestCode) {
+            case ACCESS_COARSE_LOCATION:
+            case CAMERA:
+            case ACCESS_FINE_LOCATION:
+            default:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(MainActivity.this,"kjbvkjd",Toast.LENGTH_LONG).show();
+                } else {
+                    // TODO: disable functionality that needs this permissions
+                    Toast.makeText(MainActivity.this,"Access Denied",Toast.LENGTH_LONG).show();
+                }
+                doCheckPermissions();
+                break;
+        }
+    }
 
 
     //tab on selected
