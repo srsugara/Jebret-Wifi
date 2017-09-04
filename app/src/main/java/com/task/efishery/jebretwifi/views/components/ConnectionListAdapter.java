835b641dadd26f043b1157bfae969b6bfde02cb5
srsugara <syauqisugara@gmail.com>
2017-09-05 00:30:56 +0700

    modify parameter of class

--- app/src/main/java/com/task/efishery/jebretwifi/views/components/ConnectionListAdapter.java
+++ app/src/main/java/com/task/efishery/jebretwifi/views/components/ConnectionListAdapter.java
@@ -8,36 +8,37 @@ import android.widget.BaseAdapter;
 import android.widget.TextView;
 
 import com.task.efishery.jebretwifi.R;
+import com.task.efishery.jebretwifi.models.Connection;
 import com.task.efishery.jebretwifi.views.activities.MainActivity;
+
+import java.util.List;
 
 /**
  * Created by syauqi on 04/09/17.
  */
 
-public class CustomListAdapter extends BaseAdapter{
-    String [] result;
-    int [] strength;
+public class ConnectionListAdapter extends BaseAdapter{
     Context context;
+    private List<Connection> connections;
 
     private static LayoutInflater inflater=null;
-    public CustomListAdapter(MainActivity mainActivity, String[] prgmNameList, int[] strengths) {
+    public ConnectionListAdapter(Context context, List<Connection> connections) {
         // TODO Auto-generated constructor stub
-        result=prgmNameList;
-        context=mainActivity;
-        strength=strengths;
+        this.connections=connections;
+        this.context=context;
         inflater = ( LayoutInflater )context.
                 getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
     @Override
     public int getCount() {
         // TODO Auto-generated method stub
-        return result.length;
+        return connections.size();
     }
 
     @Override
     public Object getItem(int position) {
         // TODO Auto-generated method stub
-        return position;
+        return connections.get(position);
     }
 
     @Override
@@ -50,6 +51,7 @@ public class CustomListAdapter extends BaseAdapter{
     {
         TextView tvLabel;
         SignalView signalView;
+        TextView connected;
     }
     @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
@@ -58,11 +60,20 @@ public class CustomListAdapter extends BaseAdapter{
         View rowView;
         rowView = inflater.inflate(R.layout.list_item, null);
 
+        Connection connection = (Connection) getItem(position);
+
         holder.signalView = (SignalView) rowView.findViewById(R.id.signal_view);
-        holder.signalView.setLevel(strength[position]);
+        holder.signalView.setLevel(connection.getStrength());
 
         holder.tvLabel=(TextView) rowView.findViewById(R.id.label);
-        holder.tvLabel.setText(result[position]);
+        holder.tvLabel.setText(connection.getName());
+
+        holder.connected = (TextView) rowView.findViewById(R.id.tv_connected);
+        if(connection.getIsConnected() == true ) {
+            holder.connected.setText("connected");
+        }else {
+            holder.connected.setText("");
+        }
 
         return rowView;
     }
